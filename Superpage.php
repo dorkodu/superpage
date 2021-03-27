52242dcad7e26fe7eec914e42f0ee9a360154691
Doruk Dorkodu <dorkodu@gmail.com>
2021-03-27 12:32:05 +0300

    Add getRequestMethod()

--- Superpage.php
+++ Superpage.php
@@ -190,4 +190,35 @@
 
       return $headers;
     }
+
+
+    /**
+     * Get the request method used, taking overrides into account.
+     *
+     * @return string The Request method to handle
+     */
+    public function getRequestMethod()
+    {
+      # Take the method as found in $_SERVER
+      $method = $_SERVER['REQUEST_METHOD'];
+
+      # If it's a HEAD request override it to being GET and prevent any output, as per HTTP Specification
+      # @url http://www.w3.org/Protocols/rfc2616/rfc2616-sec9.html#sec9.4
+      if ($_SERVER['REQUEST_METHOD'] == 'HEAD') {
+        ob_start();
+        $method = 'GET';
+      }
+
+      # If it's a POST request, check for a method override header
+      elseif ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $headers = $this->getRequestHeaders();
+        if (isset($headers['X-HTTP-Method-Override']) && in_array($headers['X-HTTP-Method-Override'], array('PUT', 'DELETE', 'PATCH'))) {
+            $method = $headers['X-HTTP-Method-Override'];
+        }
+      }
+
+      return $method;
+    }
+
+
   }
