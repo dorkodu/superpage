a491bc63c6cec8cb6951f28fce2cdc6077741a8f
Doruk Dorkodu <dorkodu@gmail.com>
2021-03-27 12:25:17 +0300

    Will add routes using to(...)

--- Superpage.php
+++ Superpage.php
@@ -18,4 +18,24 @@
     private $requestMethod;
 
     private $notFoundCallback;
+    
+    /**
+     * Store a route and a handling function to be executed when accessed using one of the specified methods.
+     *
+     * @param string $pattern A route pattern such as /about/company
+     * @param string $methods Allowed methods, | delimited
+     * @param callable $callback The handling function to be executed
+     */
+    public function to(string $pattern, string $methods, $callback)
+    {
+      $pattern = $this->root . '/' . trim($pattern, '/');
+      $pattern = $this->root ? rtrim($pattern, '/') : $pattern;
+
+      foreach (explode('|', $methods) as $method) {
+        $this->routes[$method][] = array(
+            'pattern' => $pattern,
+            'callback' => $callback,
+        );
+      }
+    }
   }
