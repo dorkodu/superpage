69857df4db3d3cfca730ff442a243bbbdee91db5
Doruk Dorkodu <dorkodu@gmail.com>
2021-03-27 12:23:26 +0300

    Created the Superpage class

---
+++ Superpage.php
@@ -0,0 +1,16 @@
+<?php
+
+  /**
+   * @author      Doruk Eray <doruk@dorkodu.com>
+   * @copyright   Copyright (c), 2021 Dorkodu
+   * @license     MIT public license
+   */
+  namespace Dorkodu\SuperPage;
+
+  /**
+   * A simple, callback based router.
+   */
+  class SuperPage
+  {
+    
+  }
